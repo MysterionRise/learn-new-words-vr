60635422c5fba053c0918a28b8084616d84d490a
Konstantin Perikov <pericov.ko@gmail.com>
2021-09-04 23:35:25 +0100

    update project

--- unity/Assets/RandomAudio.cs
+++ unity/Assets/RandomAudio.cs
@@ -5,26 +5,32 @@ using UnityEditor;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Valve.VR.Extras;
+using System.IO;
+using System;
 
 public class RandomAudio : MonoBehaviour
 {
-	private int count = 0;
-	private AudioSource[] audios;
+    private int count = 0;
+    private AudioSource[] audios;
     private Animator animator;
+    private bool isStarted = false;
 
     public SteamVR_LaserPointer laserPointer;
  
     private string[] lines;
     private int currentRow = 1;
+    private StreamWriter writer;
     public GameObject controller;
     public GameObject leftPicture;
     public GameObject rightPicture;
 
+    private string type = "";
     private string pic_file = "";
     private string word = "";
     private string trigger1 = "";
     private string trigger2 = "";
     private string dis = "";
+    private string correct = "";
 
     void Awake()
     {
@@ -38,10 +44,14 @@ public class RandomAudio : MonoBehaviour
         animator.SetBool("InstructionLoop", true);
         Debug.Log(audios);
         // TODO manually csv
-        TextAsset csvText = (TextAsset)Resources.Load<TextAsset>("Парадигма_test");
+        TextAsset csvText = (TextAsset)Resources.Load<TextAsset>("test");
         lines = csvText.text.Split("\n"[0]);
+
+        writer = new StreamWriter("Assets/Resources/results" + Guid.NewGuid().ToString() + ".csv");
+        writer.WriteLine("category_type,word,time,correctness");
+        writer.Flush();
         // TODO update to actual length
-        Invoke("EndInstruction", 19.0f);
+        Invoke("EndInstruction", 49.0f);
     }
 
     void Update()
@@ -56,8 +66,18 @@ public class RandomAudio : MonoBehaviour
     {
         if (e.target.name == "Da")
         {
+            if (!isStarted) 
+            {
+                isStarted = true;
+                nextRound();
+            }
+            if (isStarted) 
+            {
+                writer.WriteLine("{0},{1},{2},{3}", type, pic_file, 0, correct);
+                writer.Flush();
+            }
             Debug.Log("Da was clicked");
-            Invoke("EndInstruction", 0.0f);
+            
         } else if (e.target.name == "Net")
         {
             Debug.Log("Net was clicked");
@@ -88,17 +108,12 @@ public class RandomAudio : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-
     void nextRound() 
     {
+        animator.ResetTrigger("StartExperiment");
+        animator.SetTrigger("EndIntro");
         string[] row = lines[currentRow].Split(',');
-        string type = row[5];
+        type = row[5];
         pic_file = row[12].Replace(@"\\", @"/");;
         word = row[2].Replace(@"\\", @"/");
         if (type.Contains("VR"))
@@ -107,81 +122,78 @@ public class RandomAudio : MonoBehaviour
             if (type.Contains("импл")) {
                 trigger1 = row[17].Replace(@"\\", @"/");
                 dis = row[29].Replace(@"\\", @"/");
+                correct = row[18];
                 Invoke ("AskQuestionFM", 1.0f);
             }
             else {
                 trigger1 = row[21].Replace(@"\\", @"/");
                 trigger2 = row[25].Replace(@"\\", @"/");
                 dis = row[28].Replace(@"\\", @"/");
+                correct = row[26];
+                Debug.Log(correct);
                 Invoke ("AskQuestionEE", 1.0f);
             }
 
         }
         currentRow++;
+        writer.WriteLine("{0},{1},{2},{3}", type, pic_file, 0, correct);
+        writer.Flush();
     }
 
     void EndInstruction()
     {
+        Debug.Log("Ending instuction");
+        animator.SetBool("InstructionLoop", false);
         animator.ResetTrigger("Instruction");
         animator.SetTrigger("FinishInstruction");
-        animator.SetBool("InstructionLoop", false);
         audios[0].Stop();
         audios[1].Stop();
         audios[2].Stop();
-        // animator.ResetTrigger("FinishInstruction");
-        nextRound();
+        Invoke ("StartExperiment", 0.5f);
     }
 
+    void StartExperiment() 
+    {
+        animator.ResetTrigger("FinishInstruction");
+        audios[0].clip = (AudioClip) Resources.Load<AudioClip>("Инструкция_2");
+        audios[0].Play();
+        animator.SetTrigger("StartExperiment");
+        // TODO remove!!!!!
+        Invoke ("nextRound", 7.5f);
+        // TODO remove!!!!!
+    }
 
     void PutCardsDown()
     {
-        animator.ResetTrigger("ShowCards");
+        animator.ResetTrigger("ShowCards8sec");
         animator.SetTrigger("StopShowCards");
-        // TODO show cards with yes/no
         Invoke("PutYesNo", 0.5f);
     }
 
     void PutYesNo()
     {
         animator.ResetTrigger("StopShowCards");
-        animator.SetTrigger("PutCardsDown");
-        Invoke("ShowCards", 0.5f);
+        animator.SetTrigger("ShowCards5sec");
+        Material yes = Resources.Load<Material>("Da");
+        Material no = Resources.Load<Material>("Net");
+        leftPicture.GetComponent<MeshRenderer>().material = yes;
+        rightPicture.GetComponent<MeshRenderer>().material = no;
+        // TODO wait for actions
+        Invoke("ShowCards", 4.5f);
     }
 
 
     void ShowCards()
     {
-        animator.ResetTrigger("PutCardsDown");
-        animator.SetTrigger("StopPutCardsDown");
-        // TODO update material
-        // TODO wait 3 seconds and go next round
-        Invoke("StopShowCards", 3.0f);
-    }
-
-    void StopShowCards()
-    {
-        animator.ResetTrigger("StopPutCardsDown");
-
+        animator.ResetTrigger("ShowCards5sec");
+        animator.SetTrigger("StopShowCards");
         Invoke("nextRound", 0.5f);
     }
-
-    // Update is called once per frame
-    // void Update()
-    // {
-
-    // 	count++;
-    // 	if (count % 300 == 0) {
-            
-    //     	// Invoke ("RandomSoundness", 10);
-    //         // obj.GetComponent<MeshRenderer> ().material = materials[Random.Range(0, materials.Length)];
-    //         // TODO update image?
-    // 	}
-    // }
  
      void AskQuestionFM()
      {
         // TODO activate animation
-        animator.SetTrigger("ShowCards");
+        animator.SetTrigger("ShowCards8sec");
         Debug.Log(trigger1.Substring(0, trigger1.Length - 4));
         Debug.Log(word.Substring(0, word.Length - 4));
         AudioClip trigger1Clip = (AudioClip) Resources.Load<AudioClip>(trigger1.Substring(0, trigger1.Length - 4));
@@ -195,19 +207,17 @@ public class RandomAudio : MonoBehaviour
         leftPicture.GetComponent<MeshRenderer>().material = word_picture;
         rightPicture.GetComponent<MeshRenderer>().material = dis_picture;
 
-        // animator.SetTrigger("ShowCards");
-        //this plays the first clip and schedules the second to play immediately after it
         audios[0].clip = trigger1Clip;
         audios[1].clip = wordClip;
         audios[0].PlayScheduled(AudioSettings.dspTime + 0.1);
         audios[1].PlayScheduled(AudioSettings.dspTime + 0.0 + trigger1ClipDuration);
         // TODO 
-        Invoke ("PutCardsDown", 7.0f);
+        Invoke ("PutCardsDown", 7.5f);
      }
 
      void AskQuestionEE()
      {
-        animator.SetTrigger("ShowCards");
+        animator.SetTrigger("ShowCards8sec");
         Debug.Log(trigger1.Substring(0, trigger1.Length - 4));
         Debug.Log(trigger2.Substring(0, trigger2.Length - 4));
         Debug.Log(word.Substring(0, word.Length - 4));
@@ -230,6 +240,6 @@ public class RandomAudio : MonoBehaviour
         audios[1].PlayScheduled(AudioSettings.dspTime + 0.1 + trigger1ClipDuration);
         audios[2].PlayScheduled(AudioSettings.dspTime + 0.3 + trigger1ClipDuration + wordDuration);
         // TODO
-        Invoke ("PutCardsDown", 7.0f);
+        Invoke ("PutCardsDown", 7.5f);
      }
 }
--- unity/Assets/SteamVR_LaserPointer.cs
+++ unity/Assets/SteamVR_LaserPointer.cs
@@ -91,11 +91,13 @@ namespace Valve.VR.Extras
 
         private void Update()
         {
-            if (!isActive)
+            if(active != isActive)
             {
-                isActive = true;
-                this.transform.GetChild(0).gameObject.SetActive(true);
+                isActive = active;
+                pointer?.SetActive(isActive);
             }
+            if(!isActive)
+                return;
 
             float dist = 100f;
 
